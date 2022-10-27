015e6fc7af1cb5c16f60b2cc4d0fc115a1951494
Dominykas <110461307+Dronzillla@users.noreply.github.com>
2022-10-27 13:24:57 +0300

    Create return random answer function

--- jscript.js
+++ jscript.js
@@ -1,2 +1,18 @@
 // Test if jsciprt works
 console.log("Hello, world!");
+
+// Randomly return Rock Paper Scissors
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * (4 - 1) + 1);
+
+    if (randomNumber === 1){
+        return "Rock";
+    }
+    else if (randomNumber === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
