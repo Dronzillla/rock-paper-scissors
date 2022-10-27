a0ec6931c647c9d2b2d8431d9136c3271870f1e2
Dominykas <110461307+Dronzillla@users.noreply.github.com>
2022-10-27 13:58:38 +0300

    Add single round function

--- jscript.js
+++ jscript.js
@@ -16,3 +16,45 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+// Single round function
+function singleGame(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let output = null;
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            output = "Nobody Wins";
+        }
+        else if (computerSelection === "paper") {
+            output = "You Lose! Paper beats Rock";
+        }
+        else {
+            output = "You Win! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            output = "You Win! Paper beats Rock";
+        }
+        else if (computerSelection === "paper") {
+            output = "Nobody Wins";
+        }
+        else {
+            output = "You Lose! Scissors beats Paper";
+        }
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            output = "You Lose! Rock beats Scissors";
+        }
+        else if (computerSelection === "paper") {
+            output = "You Win! Scissors beats Paper"
+        }
+        else {
+            output = "Nobody Wins";
+        }
+    }
+    return output;
+}
